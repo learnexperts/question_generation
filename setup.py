db5c362afa851c6202ff833caec07dbf209c04c7
Garth Smedley <garth.smedley@gmail.com>
2022-05-11 17:39:20 -0300

    Make packageable

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='question-generation',
+    version='1.0.0',
+    description='Question Generation using Transformers',
+    long_description=read("README.md"),
+    python_requires='>=2.7',
+    license="MIT",
+)
+
